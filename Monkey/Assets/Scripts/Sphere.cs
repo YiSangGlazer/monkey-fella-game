aa3b55ce2e0ee07eaabded767bc6c122d5a2c3bd
YiSangGlazer <corbenwingate6@gmail.com>
2026-01-07 13:01:08 -0500

    Update3

---
+++ Monkey/Assets/Scripts/Sphere.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+
+    public Rigidbody sphere;
+    public GameObject cameraX;
+    public GameObject cameraY;
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

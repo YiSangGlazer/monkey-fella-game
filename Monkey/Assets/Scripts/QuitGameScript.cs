a02e873f7c22e6c2c03b4066a6dbd9cdea960ff0
Dante <barbenheimer37@gmail.com>
2026-01-09 12:56:50 -0500

    more button stuff

--- Monkey/Assets/Scripts/QuitGameScript.cs
+++ Monkey/Assets/Scripts/QuitGameScript.cs
@@ -1,17 +1,13 @@
 using UnityEngine;
-using UnityEngine.UI; // Needed for UI elements if interacting with them, though not strictly for quitting
+using UnityEngine.UI;
 
 public class QuitGameScript : MonoBehaviour
 {
-    // This function will be called by the UI Button
+    
     public void QuitGame()
     {
-        // For built games (Android, iOS, PC, etc.)
+        
         Application.Quit();
 
-        // For the Unity Editor (stops play mode)
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
     }
 }

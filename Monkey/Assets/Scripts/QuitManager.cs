3472943c76988552e1688f3c66b300295cd57630
Dante <barbenheimer37@gmail.com>
2026-01-14 12:55:44 -0500

    Main Menu button

--- Monkey/Assets/Scripts/QuitManager.cs
+++ Monkey/Assets/Scripts/QuitManager.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
 
-public class QuitManager : MonoBehaviour
+public class QuitGame : MonoBehaviour
 {
-    public void QuitGame()
+    public void ExitApplication()
     {
-        // This line quits the actual built application
+        // This logs a message so you know it's working while testing in the editor
+        Debug.Log("Quit button pressed!");
+
+        // This closes the actual built application
         Application.Quit();
 
-        // This line stops the "Play" mode in the Unity Editor
+        // This stops the play mode if you are running inside the Unity Editor
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
+    }
 
-        Debug.Log("Game is exiting...");
+    void Update()
+    {
+        // Optional: Quit when the 'Escape' key is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitApplication();
+        }
     }
 }

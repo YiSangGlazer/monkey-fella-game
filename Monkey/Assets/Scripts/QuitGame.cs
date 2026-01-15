f689887f9f4de9a6926e410d9f9793fd7859e77c
Dante <barbenheimer37@gmail.com>
2026-01-15 12:56:07 -0500

    Title is back

--- Monkey/Assets/Scripts/QuitGame.cs
+++ Monkey/Assets/Scripts/QuitGame.cs
@@ -1,17 +1,36 @@
 using UnityEngine;
 
-public class QuitManager : MonoBehaviour
+
+
+public class QuitGame : MonoBehaviour
 {
-    public void QuitGame()
+    public void ExitApplication()
     {
-        // This line quits the actual built application
+
+        // This logs a message so you know it's working while testing in the editor
+        Debug.Log("Quit button pressed!");
+
+        // This closes the actual built application
         Application.Quit();
 
-        // This line stops the "Play" mode in the Unity Editor
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
+        // This stops the play mode if you are running inside the Unity Editor
 
+
+    }
+
+    void Update()
+    {
+        // Optional: Quit when the 'Escape' key is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitApplication();
+        }
+
+        // This line closes the application
+        Application.Quit();
+
+        // This line only works in the Unity Editor to show that the button works
         Debug.Log("Game is exiting...");
+
     }
 }
